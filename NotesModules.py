be9e4ed6051248227286ae922af42f9cc10e6223
FrostyDot67 <anwaarnbatala@gmail.com>
2026-02-27 20:17:33 +0300

    Modules

---
+++ NotesModules.py
@@ -0,0 +1,11 @@
+"""
+Today we're dealing with modules in python, these are scripts
+with lines of codes and functions we can input into an application
+basically third party scripts which help us with our projects
+"""
+
+#Creating a module
+
+import mymodule
+
+mymodule.greeting("Anwaar")
